0beefaa7e88cc4bb75da11d7431c3f381c951fc3
Mike Nichols <mike.nichols97@gmail.com>
2018-07-16 12:54:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-let date = new Date();
+const date = new Date();
 console.log(`Twitter Bot is starting at ${date.getHours()}:${date.getMinutes()}`);
 
-let Twit = require('twit');
-let Snooper = require('reddit-snooper');
-let config = require('./configTwit');
-let configReddit = require('./configReddit');
-let download = require('image-downloader');
-let fs = require('fs');
+const Twit = require('twit');
+const Snooper = require('reddit-snooper');
+const config = require('./configTwit');
+const configReddit = require('./configReddit');
+const download = require('image-downloader');
+const fs = require('fs');
 
-let T = new Twit(config);
-let snooper = new Snooper(configReddit);
+const T = new Twit(config);
+const snooper = new Snooper(configReddit);
 
 // If counter is greater than 1 (in certain time frame) don't post tweet, less than 1 then let post ahead
 let counter = 0;
 
 getPost();
-setInterval(() => { counter = 0; }, (60000 * 50))
+setInterval(() => { counter = 0; }, (60000 * 50)) // Every 50 minutes
 
 // Gets the top post from subreddit and sends the image url to getImage()
 function getPost() {
-  let params = {
-    listing: 'hot', // Gets the posts from the "hot" index
+  const params = {
+    listing: 'top_hour', // Gets the posts from the "hot" index
     limit: 1 // Gets only 1 post
   }
 
   // Updates everytime the top post is changed
-  const subreddit = ""; // Subreddit that you would like to get picutures from (Should be a picture related subreddit)
+  const subreddit = 'pics';
   snooper.watcher.getListingWatcher(subreddit, params)
     .on('item', getImage)
     .on('error', console.error)
@@ -36,7 +36,7 @@ function getImage(item) {
   counter++;
   let picUrl = (item.data.preview.images[0].source.url);
   if(!picUrl.includes(".gif")) {
-    let options = {
+    const options = {
       url: picUrl,
       dest: "Pictures/postingImage00.jpg"
     }
@@ -57,20 +57,20 @@ function getImage(item) {
 
 // Gets the picutre that was saved in getImage() and tweets it out with
 function postPicture() {
-  let b64content = fs.readFileSync("Pictures/postingImage00.jpg", { encoding: 'base64'});
-  let random = Math.floor(Math.random() * 500);
+  const b64content = fs.readFileSync("Pictures/postingImage00.jpg", { encoding: 'base64'});
+  const random = Math.floor(Math.random() * 500);
 
   T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-    var mediaIdStr = data.media_id_string;
-    var altText = `picture num ${random}`;
-    var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+    let mediaIdStr = data.media_id_string;
+    let altText = `picture num ${random}`;
+    let meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
     T.post('media/metadata/create', meta_params, function (err, data, response) {
       if (!err) {
-        var params = { status: `${random}`, media_ids: [mediaIdStr] } // Status is the tweet's text
+        let params = { status: `${random}`, media_ids: [mediaIdStr] } // Status is the tweet's text
 
         T.post('statuses/update', params, function (err, data, response) {
-          let d2 = new Date();
+          const d2 = new Date();
           console.log(`Tweet Sent at ${d2.getHours()}:${d2.getMinutes()}`);
         })
       }
